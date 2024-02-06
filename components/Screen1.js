f6e9cc0fac9958d2bc5394ca21f33d01711f9a41
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-06 12:25:13 -0800

    Add "buttons" to change background color of chat screen with some styling

--- components/Screen1.js
+++ components/Screen1.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import { StyleSheet, View, Text, Button, TextInput, TouchableOpacity, ImageBackground } from "react-native";
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, ImageBackground } from "react-native";
 
 const Screen1 = ({ navigation }) => {
   const [name, setName] = useState("");
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={BackgroundImage.png} resizeMode="cover">
+      {/* <ImageBackground source="./assets/BackgroundImage.png" resizeMode="cover"> */}
         <Text style={styles.title}>App Title</Text>
+
         <View style={styles.box}>
           <TextInput
             style={[styles.text, styles.textInput]}
@@ -15,12 +16,21 @@ const Screen1 = ({ navigation }) => {
             onChangeText={setName}
             placeholder="Your Name"
           />
-          <Text style={styles.text}>Choose Background Color:</Text>
+
+          <Text style={styles.chooseText}>Choose Background Color:</Text>
+          {/* "buttons" to change the background color of the chat screen: */}
+          <View style={styles.colorsBox}>
+            <TouchableOpacity style={styles.color1}></TouchableOpacity>
+            <TouchableOpacity style={[styles.color1, styles.color2]}></TouchableOpacity>
+            <TouchableOpacity style={[styles.color1, styles.color3]}></TouchableOpacity>
+            <TouchableOpacity style={[styles.color1, styles.color4]}></TouchableOpacity>
+          </View>
+
           <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Screen2", {name: name})}>
             <Text style={styles.buttonText}>Start Chatting</Text>
           </TouchableOpacity>
         </View>
-      </ImageBackground>
+      {/* </ImageBackground> */}
     </View>
   );
 };
@@ -28,38 +38,67 @@ const Screen1 = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-
+    alignItems: "center",
   },
   title: {
     fontSize: 45,
     fontWeight: "600",
-    color: "#FFFFFF"
+    color: "#FFFFFF",
+    marginTop: "15%"
   },
   box: {
     width: "88%",
     height: "44%",
-    backgroundColor: "white"
+    paddingTop: 15,
+    paddingBottom: 15,
+    marginTop: "70%",
+    backgroundColor: "white",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignSelf: "center"
   },
-  text: {
+  chooseText: {
     fontSize: 16,
     fontWeight: "300",
+    marginLeft: 20,
     color: "#757083",
-    opacity: 1
+    opacity: 1,
+    alignSelf: "flex-start"
+  },
+  colorsBox: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    padding: 0,
+  },
+  color1: {
+    width: 50,
+    height: 50,
+    marginRight: 25,
+    borderRadius: 25,
+    backgroundColor: "#090c08"
+  },
+  color2: {
+    backgroundColor: "#474056"
+  },
+  color3: {
+    backgroundColor: "#8A95A5"
+  },
+  color4: {
+    backgroundColor: "#B9C6AE"
   },
   textInput: {
     width: "88%",
-    padding: 15,
+    height: 50,
+    padding: 10,
     borderWidth: 1,
-    marginTop: 15,
-    marginBottom: 15,
     opacity: 0.5
   },
   button: {
     width: "88%",
+    height: 50,
     backgroundColor: "#757083",
-    alignItems: "center"
+    alignItems: "center",
+    justifyContent: "center"
   },
   buttonText: {
     fontSize: 16,
