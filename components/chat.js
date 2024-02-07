5a1be72b5e640229e0e5e1c28d69527788adbd94
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-06 18:23:33 -0800

    Receive chatBackgroundColor

--- components/chat.js
+++ components/chat.js
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 import { StyleSheet, View, Text } from "react-native";
 
 const Chat = ({ route, navigation }) => {
-  const { name } = route.params;
+  const { name, chatBackgroundColor } = route.params;
 
   useEffect(() => {
     navigation.setOptions({ title: name })
   }, []);
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor: chatBackgroundColor}]}>
       <Text>Hello Screen 2!</Text>
     </View>
   );
