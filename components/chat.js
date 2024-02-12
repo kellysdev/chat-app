71ce7dc35e1b63735850dbf2baa9d819c5c5a759
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-12 07:58:35 -0800

    Implement GiftedChat (with KeyboardAvoidingView for android devices)

--- components/chat.js
+++ components/chat.js
@@ -1,25 +1,49 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Platform, KeyboardAvoidingView } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
+  // extract props from navigation:
   const { name, chatBackgroundColor } = route.params;
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({ title: name })
   }, []);
 
   return (
-    <View style={[styles.container, {backgroundColor: chatBackgroundColor}]}>
-      <Text>Hello Screen 2!</Text>
+    <View style={[styles.container, {backgroundColor: chatBackgroundColor}]} >
+      <GiftedChat 
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+      { Platform.OS === "android" ? <KeyboardAvoidingView behavior="height" /> : null }
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    flex: 1
   }
 });
 
