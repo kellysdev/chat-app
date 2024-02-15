a4085db4040d7242e9045aae6f13740f396c862a
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-15 08:25:37 -0800

    import getFirestore

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import Start from "./components/start";
 import Chat from "./components/chat";
 
