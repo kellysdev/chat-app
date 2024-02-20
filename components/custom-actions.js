6857ac460f4eaac8cf8f2c210e2d391626eee9f6
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-20 14:37:58 -0800

    Start CustomAction component.

---
+++ components/custom-actions.js
@@ -0,0 +1,13 @@
+import { TouchableOpacity } from "react-native";
+
+const CustomActions = () => {
+  const onActionPress = () => {}
+  
+  return (
+    <TouchableOpacity style={StyleSheet.container} onPress={onActionPress}>
+
+    </TouchableOpacity>
+  );
+};
+
+export default CustomActions;
