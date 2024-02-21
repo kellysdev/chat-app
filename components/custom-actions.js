a36a80f17018d298a866cb6992e215c5968ee329
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-20 16:43:39 -0800

    Update pickImage to save picture to Firebase Cloud Storage and to pass the image URL from Cloud Storage to the Chat component through onSend.
    Give each image uploaded a unique name consisting of the userID, the timeStamp and imageName.

--- components/custom-actions.js
+++ components/custom-actions.js
@@ -1,9 +1,10 @@
 import { TouchableOpacity, StyleSheet, View, Text, Alert } from "react-native";
 import { useActionSheet } from "@expo/react-native-action-sheet";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import * as Location from "expo-location";
 import * as ImagePicker from "expo-image-picker";
 
-const CustomActions = ({ wrapperStyle, iconTextStyle, onSend }) => {
+const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID }) => {
   const actionSheet = useActionSheet();
 
   const getLocation = async () => {
@@ -21,14 +22,31 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend }) => {
     } else Alert.alert("Permissions haven't been granted.");
   };
 
+  // generate a timestamp reference for each image to be uploaded to Firebase Cloud Storage
+  const generateReference = (uri) => {
+    const timeStamp = (new Date()).getTime();
+    const imageName = uri.split("/")[uri.split("/").length -1];
+    return `${userID}-${timeStamp}-${imageName}`;
+  };
+
+  // Expo ImagePicker requests user permission to access the device image library
+  // with permission, allows user to select an image from device and converts the image uri to a blob
+  // save image to Firebase Cloud Storage with unique name, get url and send url to Chat
   const pickImage = async () => {
     let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (permissions?.granted) {
       let result = await ImagePicker.launchImageLibraryAsync();
       if (!result.canceled) {
         const imageURI = result.assets[0].uri;
+        const uniqueRefString = generateReference(imageURI);
         const response = await fetch(imageURI);
         const blob = await response.blob();
+        const newUploadRef = ref(storage, uniqueRefString);
+        uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+          console.log("File has been uploaded successfully.");
+          const imageURL = await getDownloadURL(snapshot.ref);
+          onSend({ image: imageURL });
+        });
       }
     } else Alert.alert("Permissions haven't been granted.");
   };
